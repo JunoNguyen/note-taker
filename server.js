384c162afdd36c268f57a65a4fa1bcecbcef1414
Juno <junonguyen27@gmail.com>
2022-01-24 19:18:37 -0600

    added 404 page as well as edited server.js. mainly creating routes and defining the required'

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
